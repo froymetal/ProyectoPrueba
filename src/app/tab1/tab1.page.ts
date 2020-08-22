3ab0b1586527329db79fe3877543ea922b50d051
Xtinction Software <xtinction.software@gmail.com>
2020-08-21 21:22:21 -0500

    prueba sqlite

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataLocalService } from '../services/data-local.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private dataLocalService: DataLocalService) { }
+  
+  guardar() {
+    this.dataLocalService.guardarInfo('prueba2');
+  }
+
+
 
 }
